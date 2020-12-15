996f4f34b8db45acf3aff09f96c066b078cfab24
ferror18 <ferror18@gmail.com>
2020-12-15 09:13:03 -0800

    Initial commit

---
+++ dataGenetator.js
@@ -0,0 +1,126 @@
+const faker = require("faker");
+const jsonfile = require("jsonfile");
+
+const file = "./test.json";
+
+let data = jsonfile.readFileSync(file);
+data = {};
+
+const NUMOFLIBS = 1;
+const NUMOFHEADS = NUMOFLIBS * 3;
+let NUMOFSTUDENTS = 0;
+
+//Librarys
+data.library = [];
+for (let index = 0; index < NUMOFLIBS; index++) {
+  //Generate data
+  let fakeLib = {
+    id: index,
+    name: faker.company.companyName(1),
+    description: faker.random.words(30),
+    library_usage: faker.random.words(30),
+    notes: faker.random.words(30),
+    image: faker.image.imageUrl(600, 600),
+    villageId: index,
+  };
+
+  data.library.push(fakeLib);
+}
+console.log(data.library, "\n\n\n\n\n\n\n\n\n\n\n");
+
+//Villages
+data.village = [];
+for (let index = 0; index < NUMOFLIBS; index++) {
+  //Generate data
+  let fakeVillage = {
+    id: index,
+    name: faker.address.city(),
+    GPS_coordinates: faker.address.nearbyGPSCoordinate(),
+    village_contact_name: faker.name.findName(),
+    village_contact_phone: faker.phone.phoneNumberFormat(),
+    notes: faker.random.words(50),
+    libraryId: index,
+    headmasterId: faker.random.number(NUMOFLIBS),
+  };
+
+  data.village.push(fakeVillage);
+}
+console.log(data.village, "\n\n\n\n\n\n\n\n\n\n\n");
+
+//Schools
+data.school = [];
+for (let index = 0; index < NUMOFLIBS; index++) {
+  //Generate data
+  //   let schoolStudents = faker.random.number(NUMOFHEADS * 3, NUMOFHEADS * 25);
+  let schoolStudents = faker.random.number(NUMOFHEADS * 15) + NUMOFHEADS * 3;
+  NUMOFSTUDENTS += schoolStudents;
+  let fakeSchool = {
+    id: index,
+    name: faker.company.companyName(),
+    num_students: faker.random.number(schoolStudents),
+    num_teachers: faker.random.number(NUMOFHEADS) + 4,
+    school_description: faker.random.words(30),
+    school_needs: faker.random.words(30),
+    school_goals: faker.random.words(30),
+  };
+
+  //   for (let x = 0; x < 3; x++) {
+  //       fakeSchool.dynamic_questions.push({`${faker.hacker.phrase()}`: faker.hacker.phrase()})
+  //   }
+
+  data.school.push(fakeSchool);
+}
+console.log(data.school, "\n\n\n\n\n\n\n\n\n\n\n");
+
+//Headmasters
+data.headmaster = [];
+for (let index = 0; index < NUMOFHEADS; index++) {
+  //Generate data
+  let fakeHeadmaster = {
+    id: index,
+    name: faker.name.findName(),
+    gender: faker.name.gender(),
+    address: faker.address.streetAddress(),
+    GPS_coordinates: faker.address.nearbyGPSCoordinate(),
+    images_drive_folder_link: faker.internet.url(),
+    headmasters_picture: faker.image.imageUrl(),
+    education_contact: {
+      name: faker.name.findName(),
+      phone: faker.phone.phoneNumberFormat(2),
+      email: faker.internet.email(),
+      jobTitle: faker.name.jobTitle(),
+    },
+    notes: faker.random.words(50),
+  };
+
+  data.headmaster.push(fakeHeadmaster, "\n\n\n\n\n\n\n\n\n\n\n");
+}
+
+// //Students
+// data.headmaster = [];
+// for (let index = 0; index < NUMOFLIBS * 5; index++) {
+//   //Generate data
+//   let randomVillage = faker.random.number(NUMOFLIBS);
+//   let fakeHeadmaster = {
+//     id: index,
+//     name: faker.name.findName(),
+//     gender: faker.name.gender(),
+//     address: faker.address.streetAddress(),
+//     GPS_coordinates: faker.address.nearbyGPSCoordinate(),
+//     images_drive_folder_link: faker.internet.url(),
+//     headmasters_picture: faker.image.imageUrl(),
+//     education_contact: {
+//       name: faker.name.findName(),
+//       phone: faker.phone.phoneNumberFormat(2),
+//       email: faker.internet.email(),
+//       jobTitle: faker.name.jobTitle(),
+//     },
+//     notes: faker.random.words(50),
+//     libraryId: randomVillage,
+//     villageId: randomVillage,
+//   };
+
+//   data.headmaster.push(fakeHeadmaster, "\n\n\n\n\n\n\n\n\n\n\n");
+// }
+
+jsonfile.writeFileSync(file, data);
